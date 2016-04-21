93c5ca4044db6b0d8f93cfb365c2978d5f435d51
codematrix <ross_pellegrino@hotmail.com>
2016-04-21 10:16:01 -0400

    CallT-402 - added aggregated exceptions of failed connections.

--- src/Plato.Messaging/Implementations/AMQ/Factories/AMQConnectionFactory.cs
+++ src/Plato.Messaging/Implementations/AMQ/Factories/AMQConnectionFactory.cs
@@ -37,48 +37,52 @@ namespace Plato.Messaging.Implementations.AMQ.Factories
         /// <exception cref="MessageException">
         /// </exception>
         public IConnection CreateConnection(string name)
-        {            
+        {
+            var exceptionList = new List<Exception>();
             var connectionSettings = _configManager.GetConnectionSettings(name);
 
-            if(string.IsNullOrWhiteSpace(connectionSettings.Uri))
+            if (string.IsNullOrWhiteSpace(connectionSettings.Uri))
             {
-                throw new MessageException(MessageExceptionCode.UnhandledError, $"Missing or empty connection Uri for named connection settings: '{name}'.");
+                exceptionList.Add(new MessageException(MessageExceptionCode.UnhandledError, $"Missing or empty connection Uri for named connection settings: '{name}'."));
             }
-
-            foreach (var uri in connectionSettings.Uri.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            else
             {
-                try
+                foreach (var uri in connectionSettings.Uri.Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    var connectionUri = new Uri(uri);
-                    var connectionFactory = new ConnectionFactory(connectionUri)
+                    try
                     {
-                        UserName = connectionSettings.Username,
-                        Password = connectionSettings.Password,
-                        AsyncSend = connectionSettings.AsyncSend,
-                    };
+                        var connectionUri = new Uri(uri);
+                        var connectionFactory = new ConnectionFactory(connectionUri)
+                        {
+                            UserName = connectionSettings.Username,
+                            Password = connectionSettings.Password,
+                            AsyncSend = connectionSettings.AsyncSend,
+                        };
 
-                    var connection = connectionFactory.CreateConnection();
-                    return connection;                    
-                }
-                catch (NMSConnectionException)
-                {
-                    // swallow
-                }
-                catch (InvalidClientIDException)
-                {
-                    // swallow
-                }
-                catch (NMSException)
-                {
-                    // swallow
-                }
-                catch (UriFormatException ex)
-                {
-                    throw new MessageException(MessageExceptionCode.UnhandledError, ex.Message, ex);
+                        var connection = connectionFactory.CreateConnection();
+                        return connection;
+                    }
+                    catch (NMSConnectionException ex)
+                    {
+                        exceptionList.Add(ex);
+                    }
+                    catch (InvalidClientIDException ex)
+                    {
+                        exceptionList.Add(ex);
+                    }
+                    catch (NMSException ex)
+                    {
+                        exceptionList.Add(ex);
+                    }
+                    catch (UriFormatException ex)
+                    {
+                        exceptionList.Add(ex);
+                    }
                 }
             }
 
-            throw new MessageException(MessageExceptionCode.LostConnection, $"Unable to connect to any ActiveMQ Broker using the following connection uri: {connectionSettings.Uri}, for named connection settings: '{name}'.");
+            var finalException = new AggregateException($"Unable to connect to any ActiveMQ Broker using the following connection uri: { connectionSettings.Uri}, for named connection settings: '{name}'.", exceptionList);
+            throw new MessageException(MessageExceptionCode.LostConnection, finalException.Message, finalException);
         }
     }
 }
