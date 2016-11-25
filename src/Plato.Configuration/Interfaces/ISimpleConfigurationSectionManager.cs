d0c1f4b91ee6e6a16779ad8d6a17d4ae00ad9570
codematrix <ross_pellegrino@hotmail.com>
2016-11-25 10:32:09 -0500

    Extended SimpleConfigurationSectionManager.

--- src/Plato.Configuration/Interfaces/ISimpleConfigurationSectionManager.cs
+++ src/Plato.Configuration/Interfaces/ISimpleConfigurationSectionManager.cs
@@ -2,6 +2,7 @@
 // Copyright (c) 2016 ReflectSoftware Inc.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
+using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace Plato.Configuration.Interfaces
@@ -11,6 +12,14 @@ namespace Plato.Configuration.Interfaces
     /// </summary>
     public interface ISimpleConfigurationSectionManager
     {
+        /// <summary>
+        /// Gets the node attributes.
+        /// </summary>
+        /// <value>
+        /// The node attributes.
+        /// </value>
+        NodeChildAttributes NodeAttributes { get; }
+
         /// <summary>
         /// Gets the attribute.
         /// </summary>
@@ -28,5 +37,20 @@ namespace Plato.Configuration.Interfaces
         /// <param name="name">The name.</param>
         /// <returns></returns>
         NameValueCollection GetAttributes(string nodeName, string name);
+
+        /// <summary>
+        /// Gets the attribute values.
+        /// </summary>
+        /// <param name="nodeName">Name of the node.</param>
+        /// <param name="attribute">The attribute.</param>
+        /// <returns></returns>
+        IEnumerable<string> GetAttributeValues(string nodeName, string attribute);
+
+        /// <summary>
+        /// Gets the attribute values.
+        /// </summary>
+        /// <param name="attribute">The attribute.</param>
+        /// <returns></returns>
+        IEnumerable<string> GetAttributeValues(string attribute);
     }
 }
