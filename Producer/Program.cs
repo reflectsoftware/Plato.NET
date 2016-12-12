1095b032333fbb545bb8d972c416b6d979300c62
codematrix <ross_pellegrino@hotmail.com>
2016-12-12 08:48:53 -0500

    Modifying RMQ

---
+++ Producer/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Plato.Messaging.Implementations.RMQ;
+using Plato.Messaging.Implementations.RMQ.Factories;
+using Plato.Messaging.Implementations.RMQ.Interfaces;
+using Plato.Messaging.Implementations.RMQ.Settings;
+using Plato.Messaging.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRMQConfigurationManager _configurationManager = new RMQConfigurationManager();
+            IRMQConnectionManager _connectionManager = new RMQConnectionManager(_configurationManager);
+            IRMQConnectionFactory _connectionFactory = new RMQConnectionFactory(_connectionManager);
+
+            var rmqSettings = new RMQSettings()
+            {
+                ConnectionName = "defaultConnection",
+                ConnectionFactory = _connectionFactory,
+                ExchangeSettings  = _configurationManager.GetExchangeSettings("test")
+            };
+                       
+
+            var senderFactory = new RMQSenderFactory();
+            var sender = senderFactory.CreateText(rmqSettings);
+
+            var sample =  new
+            {
+                Name = "Ross",
+                Location = "Toronto"
+            };
+
+            sender.Send( JsonConvert.SerializeObject(sample));
+
+            //while (true)
+            //{
+            //    var key = Console.ReadKey();
+            //    if(key.KeyChar == 'q')
+            //    {
+            //        break;
+            //    }
+            //}
+
+            sender.Dispose();
+            _connectionManager.Dispose();
+        }
+    }
+}
