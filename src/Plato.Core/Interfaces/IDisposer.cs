00bb78c09d5bdf866bcdda27a634e615654be1ed
codematrix <ross_pellegrino@hotmail.com>
2016-12-28 18:41:44 -0500

    Adding a Disposer helper class.

---
+++ src/Plato.Core/Interfaces/IDisposer.cs
@@ -0,0 +1,19 @@
+﻿// Plato.NET
+// Copyright (c) 2016 ReflectSoftware Inc.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+
+using System;
+
+namespace Plato.Core.Interfaces
+{
+    public interface IDisposer<T> : IDisposable where T : class
+    {
+        bool Disposed { get; }
+        T Instance { get; }
+    }
+
+    public interface IDisposerFactory
+    {
+        IDisposer<T> Create<T>(T instance) where T : class;
+    }
+}
---
+++ src/Plato.Core/Miscellaneous/Disposer.cs
@@ -0,0 +1,88 @@
+﻿// Plato.NET
+// Copyright (c) 2016 ReflectSoftware Inc.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+
+using Plato.Core.Interfaces;
+using System;
+
+namespace Plato.Core.Miscellaneous
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="Plato.Core.Interfaces.IDisposer{T}" />
+    public class Disposer<T> : IDisposer<T> where T : class
+    {
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="Disposer{T}"/> is disposed.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if disposed; otherwise, <c>false</c>.
+        /// </value>
+        public bool Disposed { get; private set; }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>
+        /// The instance.
+        /// </value>
+        public T Instance { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Disposer{T}"/> class.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        public Disposer(T instance)
+        {
+            Disposed = false;
+            Instance = instance;
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="Disposer{T}"/> class.
+        /// </summary>
+        ~Disposer()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            lock (this)
+            {
+                if (!Disposed)
+                {
+                    Disposed = true;
+                    if (disposing)
+                    {
+                        GC.SuppressFinalize(this);
+                    }
+
+                    (Instance as IDisposable)?.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+
+    public class DisposerFactory : IDisposerFactory
+    {
+        public IDisposer<T> Create<T>(T instance) where T : class
+        {
+            return new Disposer<T>(instance);
+        }
+    }
+}
