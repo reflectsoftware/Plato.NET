c15a290158b8ad3504715422767365c44a92657a
Callon Campbell <calloncampbell@msn.com>
2017-01-02 21:49:41 -0500

    Updated copyright to 2017 in code headers and nuspec files.

--- src/Plato/Properties/AssemblyInfo.cs
+++ src/Plato/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿// Plato.NET
+// Copyright (c) 2017 ReflectSoftware Inc.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
--- src/Plato/Threading/WorkManagement/WorkManagerNotification.cs
+++ src/Plato/Threading/WorkManagement/WorkManagerNotification.cs
@@ -1,5 +1,5 @@
 ﻿// Plato.NET
-// Copyright (c) 2016 ReflectSoftware Inc.
+// Copyright (c) 2017 ReflectSoftware Inc.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
 using Plato.ExceptionManagement;
