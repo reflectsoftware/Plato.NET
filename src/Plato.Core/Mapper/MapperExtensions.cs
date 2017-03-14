da0ffb7d369ac3018d528e02ee982e3423eb4dfc
codematrix <ross_pellegrino@hotmail.com>
2017-03-14 18:55:06 -0400

    Added Mapper and updated some nuget packages

--- src/Plato.Core/Mapper/MapperExtensions.cs
+++ src/Plato.Core/Mapper/MapperExtensions.cs
@@ -14,14 +14,16 @@ namespace Plato.Core.Mapper
         /// <typeparam name="TSource">The type of the source.</typeparam>
         /// <typeparam name="TTarget">The type of the target.</typeparam>
         /// <param name="source">The source.</param>
+        /// <param name="addition">The addition.</param>
         /// <param name="args">The arguments.</param>
         /// <returns></returns>
-        public static TTarget Map<TMapper, TSource, TTarget>(this TSource source, params Object[] args) where TMapper : IMapper<TSource, TTarget>
+        public static TTarget Map<TMapper, TSource, TTarget>(this TSource source, Action<TSource, TTarget> addition = null, params object[] args) where TMapper : IMapper<TSource, TTarget>
         {
             var map = (TMapper)Activator.CreateInstance(typeof(TMapper), args);
             var target = Activator.CreateInstance<TTarget>();
 
-            map.Map(source, target);
+            target = map.Map(source, target);
+            addition?.Invoke(source, target);
 
             return target;
         }
