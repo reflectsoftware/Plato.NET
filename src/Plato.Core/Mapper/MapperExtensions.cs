7e26580e04f095a3dc0069511ac288f422ec605f
codematrix <ross_pellegrino@hotmail.com>
2017-03-17 14:19:28 -0400

    Added Mapper and updated some nuget packages

--- src/Plato.Core/Mapper/MapperExtensions.cs
+++ src/Plato.Core/Mapper/MapperExtensions.cs
@@ -8,6 +8,79 @@ namespace Plato.Core.Mapper
     public static class MapperExtensions
     {
         /// <summary>
+        /// Maps the specified source.
+        /// </summary>
+        /// <typeparam name="TMapper">The type of the mapper.</typeparam>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TTarget">The type of the target.</typeparam>
+        /// <param name="mapper">The mapper.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="target">The target.</param>
+        /// <returns></returns>
+        public static TTarget Map<TMapper, TSource, TTarget>(this TMapper mapper, TSource source, TTarget target)
+            where TSource : class
+            where TTarget : class
+            where TMapper : IMapper<TSource, TTarget>
+        {
+            mapper.Map(source, target);
+            return target;
+        }
+
+        /// <summary>
+        /// Maps the specified source.
+        /// </summary>
+        /// <typeparam name="TMapper">The type of the mapper.</typeparam>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TTarget">The type of the target.</typeparam>
+        /// <param name="mapper">The mapper.</param>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        public static TTarget Map<TMapper, TSource, TTarget>(this TMapper mapper, TSource source)
+            where TSource : class
+            where TTarget : class
+            where TMapper : IMapper<TSource, TTarget>
+        {
+            var target = Activator.CreateInstance<TTarget>();
+            return mapper.Map<TMapper, TSource, TTarget>(source, target);
+        }
+
+        /// <summary>
+        /// Maps the specified mapper.
+        /// </summary>
+        /// <typeparam name="TMapper">The type of the mapper.</typeparam>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TTarget">The type of the target.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="mapper">The mapper.</param>
+        /// <param name="target">The target.</param>
+        /// <returns></returns>
+        public static TTarget Map<TMapper, TSource, TTarget>(this TSource source, TMapper mapper, TTarget target)
+            where TSource : class
+            where TTarget : class
+            where TMapper : IMapper<TSource, TTarget>
+        {
+            return mapper.Map<TMapper, TSource, TTarget>(source, target);
+        }
+
+        /// <summary>
+        /// Maps the specified mapper.
+        /// </summary>
+        /// <typeparam name="TMapper">The type of the mapper.</typeparam>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TTarget">The type of the target.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="mapper">The mapper.</param>
+        /// <returns></returns>
+        public static TTarget Map<TMapper, TSource, TTarget>(this TSource source, TMapper mapper)
+            where TSource : class
+            where TTarget : class
+            where TMapper : IMapper<TSource, TTarget>
+        {
+            var target = Activator.CreateInstance<TTarget>();
+            return mapper.Map<TMapper, TSource, TTarget>(source, target);
+        }
+
+        /// <summary>
         /// Maps the specified target.
         /// </summary>
         /// <typeparam name="TMapper">The type of the mapper.</typeparam>
@@ -15,36 +88,32 @@ namespace Plato.Core.Mapper
         /// <typeparam name="TTarget">The type of the target.</typeparam>
         /// <param name="source">The source.</param>
         /// <param name="target">The target.</param>
-        /// <param name="args">The arguments.</param>
         /// <returns></returns>
-        public static TTarget Map<TMapper, TSource, TTarget>(this TSource source, TTarget target, params object[] args)
+        public static TTarget Map<TMapper, TSource, TTarget>(this TSource source, TTarget target)
             where TSource : class
             where TTarget : class
             where TMapper : IMapper<TSource, TTarget>
         {
-            var map = (TMapper)Activator.CreateInstance(typeof(TMapper), args);
-
-            map.Map(source, target);
-
-            return target;
+            var mappper = (TMapper)Activator.CreateInstance(typeof(TMapper));
+            return mappper.Map<TMapper, TSource, TTarget>(source, target);
         }
 
         /// <summary>
-        /// Maps the specified arguments.
+        /// Maps the specified source.
         /// </summary>
         /// <typeparam name="TMapper">The type of the mapper.</typeparam>
         /// <typeparam name="TSource">The type of the source.</typeparam>
         /// <typeparam name="TTarget">The type of the target.</typeparam>
         /// <param name="source">The source.</param>
-        /// <param name="args">The arguments.</param>
         /// <returns></returns>
-        public static TTarget Map<TMapper, TSource, TTarget>(this TSource source, params object[] args)
+        public static TTarget Map<TMapper, TSource, TTarget>(this TSource source)
             where TSource : class
             where TTarget : class
             where TMapper : IMapper<TSource, TTarget>
         {
             var target = Activator.CreateInstance<TTarget>();
-            return source.Map<TMapper, TSource, TTarget>(target, args);
+            return source.Map<TMapper, TSource, TTarget>(target);
         }
+
     }
 }
