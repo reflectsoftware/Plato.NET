0c6e6072dc366b82e7408dc68bf2f78150b028fb
codematrix <ross_pellegrino@hotmail.com>
2018-05-01 17:47:02 -0400

    Nuget Update

---
+++ src/Plato.Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Plato.Core.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Determines whether [is null or empty].
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable">The enumerable.</param>
+        /// <returns>
+        ///   <c>true</c> if [is null or empty] [the specified enumerable]; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || enumerable.Count() == 0;
+        }
+    }
+}
---
+++ src/Plato.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Plato.Core.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Stripped the specified unique identifier.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <returns></returns>
+        public static string Stripped(this Guid guid)
+        {
+            return guid.ToString().Replace("-", string.Empty).ToUpper();
+        }
+    }
+}
